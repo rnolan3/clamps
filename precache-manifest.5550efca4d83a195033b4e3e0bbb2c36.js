37bb660d795c9145399db11a197a71785fcc7563
Robert Nolan <rwnolan@customerdirect.com>
2018-12-19 22:00:24 -0600

    Updates

---
+++ precache-manifest.5550efca4d83a195033b4e3e0bbb2c36.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9fadeb8954ee7dee3182",
+    "url": "/clamps/static/js/runtime~main.9fadeb89.js"
+  },
+  {
+    "revision": "1ff7e5c1f789f96986ac",
+    "url": "/clamps/static/js/main.1ff7e5c1.chunk.js"
+  },
+  {
+    "revision": "3413503b86ea1252dd27",
+    "url": "/clamps/static/js/1.3413503b.chunk.js"
+  },
+  {
+    "revision": "1ff7e5c1f789f96986ac",
+    "url": "/clamps/static/css/main.f0361372.chunk.css"
+  },
+  {
+    "revision": "40f2cd145ec2fd12e5fb06318b2661c4",
+    "url": "/clamps/index.html"
+  }
+];
